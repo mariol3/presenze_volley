087df5d86e7f01a0b72c3c26587ec20b922f93a8
Mario Grimaldi <mario.grimaldi89@gmail.com>
2013-11-22 20:21:05 +0100

    Basic views for players and trainings

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,15 @@
 class Player < ActiveRecord::Base
+  before_save :default_values
   has_many :participation
   has_many :training, through: :participation
   
   validates :name, presence: true, uniqueness: true
   
   has_secure_password
+  
+  def default_values
+    if (!self.avatar_url || self.avatar_url.empty?)
+      self.avatar_url = 'default_avatar.png'
+    end
+  end
 end
