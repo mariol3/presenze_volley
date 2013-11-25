be58421cbf8ea9282fd0f18dc57ded88974efa33
Mario Grimaldi <mario.grimaldi89@gmail.com>
2013-11-25 20:19:45 +0100

    Handling no training in next training page

--- app/controllers/next_training_controller.rb
+++ app/controllers/next_training_controller.rb
@@ -1,14 +1,16 @@
 class NextTrainingController < ApplicationController
   def index
     @next_training = Training.order(:date).first
-    @participants_players = @next_training.participations.where(present: true).count
-    @bar_color_class =  if @participants_players < 10
-                          'progress-danger'
-                        elsif @participants_players < 12
-                          'progress-warning'
-                        else
-                          'progress-success'
-                        end
-    @players_percentage = (@participants_players * 100.0 / Player.all.count).round
+    if @next_training
+      @participants_players = @next_training.participations.where(present: true).count
+      @bar_color_class =  if @participants_players < 10
+                            'progress-danger'
+                          elsif @participants_players < 12
+                            'progress-warning'
+                          else
+                            'progress-success'
+                          end
+      @players_percentage = (@participants_players * 100.0 / Player.all.count).round
+    end
   end
 end
