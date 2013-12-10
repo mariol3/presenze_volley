16c7723d367de125eefea1f5ec3393967d6d41c9
Mario Grimaldi <m.grimaldi@addiction.it>
2013-12-10 16:39:00 +0100

    list of participant on next training

--- app/controllers/next_training_controller.rb
+++ app/controllers/next_training_controller.rb
@@ -2,15 +2,16 @@ class NextTrainingController < ApplicationController
   def index
     @next_training = Training.where("date >= ?", Date.today).order(:date).first
     if @next_training
-      @participants_players = @next_training.participations.where(present: true).count
-      @bar_color_class =  if @participants_players < 10
+      @all_participants = @next_training.participations.order('present DESC')
+      @participants_players = @next_training.participations.where(present: true)
+      @bar_color_class =  if @participants_players.count < 10
                             'progress-danger'
-                          elsif @participants_players < 12
+                          elsif @participants_players.count < 12
                             'progress-warning'
                           else
                             'progress-success'
                           end
-      @players_percentage = (@participants_players * 100.0 / Player.all.count).round
+      @players_percentage = (@participants_players.count * 100.0 / Player.all.count).round
     end
   end
 end
