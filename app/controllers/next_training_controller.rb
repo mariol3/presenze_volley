cb5b844a9a5916c05764e200430eb797faba3ac8
Mario Grimaldi <mario.grimaldi89@gmail.com>
2013-12-10 17:45:07 +0100

    Caption for table listing participants. Ordering for participants.

--- app/controllers/next_training_controller.rb
+++ app/controllers/next_training_controller.rb
@@ -2,7 +2,7 @@ class NextTrainingController < ApplicationController
   def index
     @next_training = Training.where("date >= ?", Date.today).order(:date).first
     if @next_training
-      @all_participants = @next_training.participations.order('present DESC')
+      @all_participants = @next_training.participations.order('created_at ASC, present DESC')
       @participants_players = @next_training.participations.where(present: true)
       @bar_color_class =  if @participants_players.count < 10
                             'progress-danger'
