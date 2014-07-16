6e3eceebf2774f6cb46cd0de022e26eb0fb3c597
Mario Grimaldi <mario.grimaldi89@gmail.com>
2014-07-16 19:29:07 +0200

    Fixed hide popover on popover content click. Returned to old participations view.

--- app/controllers/next_training_controller.rb
+++ app/controllers/next_training_controller.rb
@@ -2,8 +2,9 @@ class NextTrainingController < ApplicationController
   def index
     @next_training = Training.where("date >= ?", Date.today).order(:date).first
     if @next_training
-      @all_participants = @next_training.participations.order('present DESC, created_at ASC')
       @participants_players = @next_training.participations.where(present: true)
+      @not_participants_players = @next_training.participations.where(present: false)
+
       @bar_color_class =  if @participants_players.count < 10
                             'progress-danger'
                           elsif @participants_players.count < 12
@@ -12,8 +13,8 @@ class NextTrainingController < ApplicationController
                             'progress-success'
                           end
       @players_percentage = (@participants_players.count * 100.0 / 12).round
-      
-      all_participants_players = @participants_players.map {|p| p.player}
+
+      all_participants_players = @next_training.participations.order('present DESC, created_at ASC').map {|p| p.player}
       @not_confirmed_players = Player.where.not(id: all_participants_players.collect(&:id)).order(:name)
     end
   end
